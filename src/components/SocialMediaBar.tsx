2906fdc9d1d8fd97f84a810eae37aa2e1f52282f
Augusto Barco <augustopayza@gmail.com>
2021-01-14 19:53:28 -0400

    ⚡️ applying relative sizes

--- src/components/SocialMediaBar.tsx
+++ src/components/SocialMediaBar.tsx
@@ -4,10 +4,10 @@ import { Facebook } from 'assets/socialMediaIcons';
 export default function SocialMediaBar() {
   return (
     <div className="flex justify-center items-center svg:m-4">
-      <Icon name="facebook" className="mr-3" />
-      <Icon name="twitter" className="mx-3" />
-      <Icon name="github" className="mx-3" />
-      <Icon name="linked-in" className="ml-3" />
+      <Icon name="facebook" className="mr-3" width="2.5rem" />
+      <Icon name="twitter" className="mx-3" width="2.5rem" />
+      <Icon name="github" className="mx-3" width="2.5rem" />
+      <Icon name="linked-in" className="ml-3" width="2.5rem" />
     </div>
   );
 }
