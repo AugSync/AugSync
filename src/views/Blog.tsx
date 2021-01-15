cfc260a05ee85330c39755608b252b74c76a3dfa
Augusto Barco <augustopayza@gmail.com>
2021-01-15 11:35:22 -0400

    ⚡️ command line component ready

--- src/views/Blog.tsx
+++ src/views/Blog.tsx
@@ -1,10 +1,33 @@
+import CommandLine from 'components/CommandLine';
 import Header from 'components/Header';
 
 export default function Blog() {
+  const commandLineValues = [
+    { type: 'home', title: '~/' },
+    { type: 'folder', title: '.home' },
+    { type: 'split', title: '/' },
+    { type: 'folder', title: 'blog', color: 'text-orange-dark' },
+    { type: 'input', title: '>' },
+    { type: 'output', title: 'cat welcome.txt' },
+    { type: 'break' },
+    { type: 'home', title: '~/' },
+    { type: 'folder', title: '.home' },
+    { type: 'split', title: '/' },
+    { type: 'folder', title: 'blog', color: 'text-orange-dark' },
+    { type: 'input', title: '>' },
+    {
+      type: 'output',
+      title:
+        'Welcome to my blog, a place where you can find all kinds of articles on application development in multiple programming languages. Sometimes I write about my experience in multiple areas of programming and learning, also about emotional intelligence in the workplace, among other super interesting topics.',
+      color: 'text-blue',
+      isResponse: true,
+    },
+  ];
+
   return (
     <>
       <Header />
-      <h1>Blog</h1>
+      <CommandLine values={commandLineValues} />
     </>
   );
 }
--- src/views/Portfolio.tsx
+++ src/views/Portfolio.tsx
@@ -1,10 +1,33 @@
+import CommandLine from 'components/CommandLine';
 import Header from 'components/Header';
 
 export default function Portfolio() {
+  const commandLineValues = [
+    { type: 'home', title: '~/' },
+    { type: 'folder', title: '.home' },
+    { type: 'split', title: '/' },
+    { type: 'folder', title: 'portfolio', color: 'text-orange-dark' },
+    { type: 'input', title: '>' },
+    { type: 'output', title: 'cat welcome.txt' },
+    { type: 'break' },
+    { type: 'home', title: '~/' },
+    { type: 'folder', title: '.home' },
+    { type: 'split', title: '/' },
+    { type: 'folder', title: 'portfolio', color: 'text-orange-dark' },
+    { type: 'input', title: '>' },
+    {
+      type: 'output',
+      title:
+        'Welcome to my portfolio, a place where you can find all kinds of programming projects in any language. I would love for you to see my projects where I try to capture everything I learn in this industry, lets enjoy my projects.',
+      color: 'text-blue',
+      isResponse: true,
+    },
+  ];
+
   return (
     <>
       <Header />
-      <h1>Portfolio</h1>
+      <CommandLine values={commandLineValues} />
     </>
   );
 }
