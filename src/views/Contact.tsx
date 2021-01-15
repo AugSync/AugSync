ca76fe049e67deffab29230a6cbc9b2f1cf64887
Augusto Barco <augustopayza@gmail.com>
2021-01-15 19:38:06 -0400

    ⚡️ contact cover added

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -1,10 +1,18 @@
 import Header from 'components/Header';
+import Image from 'next/image';
 
 export default function Contact() {
   return (
     <>
       <Header />
-      <h1>Contact</h1>
+      <div className="relative w-full" style={{ height: '30rem' }}>
+        <Image
+          src="/images/contact.jpg"
+          alt="Person contacting to anybody"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
     </>
   );
 }
