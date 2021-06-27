d2d4c57f6790bf3046fe269a0ac396b6fb00f27d
Augusto Barco <augustopayza@gmail.com>
2021-06-27 07:50:28 -0400

    create hero

--- src/views/Blog/Article.tsx
+++ src/views/Blog/Article.tsx
@@ -4,13 +4,13 @@ import Footer from 'components/Footer';
 import React from 'react';
 import Hero from 'components/Hero';
 
-export default function Contact() {
+export default function Article() {
   return (
     <>
       <Header />
       <Hero />
-      <h1 className="my-10 text-xl text-center font-play text-blue">
-        Contact Me
+      <h1 className="my-10 text-xl text-center font-play text-orange-dark">
+        This is a article
       </h1>
       <Footer />
     </>
