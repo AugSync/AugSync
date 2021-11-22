154cce1412cef2f3ac0152c6809ea54334aa5cb4
Augusto Barco <augustopayza@gmail.com>
2021-11-22 10:33:44 -0400

    hotfix: Repair markdown module

--- src/views/Blog/Article.tsx
+++ src/views/Blog/Article.tsx
@@ -5,59 +5,12 @@ import React from 'react';
 import Hero from 'components/Hero';
 import Icon from 'assets/icons';
 import { PoweredByBadge } from 'components/Badges';
-import ReactMarkdown from 'react-markdown';
+import MarkdownView from 'react-showdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import clsx from 'clsx';
 
-// Did you know you can use tildes instead of backticks for code in markdown? ✨
-const markdown = `### This is a Sub-Heading 
-
-Contrary to popular belief, Lorem Ipsum is not simply random text. It
-has roots in a piece of classical Latin literature from 45 BC, making
-it over 2000 years old. Richard McClintock, a Latin professor at
-Hampden-Sydney College in Virginia, looked up one of the more obscure
-Latin words, consectetur, from a Lorem Ipsum passage, and going
-through the cites of the word in classical literature, discovered the
-undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
-Evil) by Cicero, written in 45 BC. This book is a treatise on the
-theory of ethics, very popular during the Renaissance. The first line
-of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
-section 1.10.32. The standard chunk of Lorem Ipsum used since the
-1500s is reproduced below for those interested. Sections 1.10.32 and
-1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
-reproduced in their exact original form, accompanied by English
-versions from the 1914 translation by H. Rackham.
-
-~~~js
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-const Component = () => {
-  const codeString = '(num) => num + 1';
-  return (
-    <SyntaxHighlighter language="javascript" style={docco}>
-      {codeString}
-    </SyntaxHighlighter>
-  );
-};
-~~~
-
-Latin words, consectetur, from a Lorem Ipsum passage, and going
-through the cites of the word in classical literature, discovered the
-undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
-Evil) by Cicero, written in 45 BC. This book is a treatise on the
-theory of ethics, very popular during the Renaissance. The first line
-of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
-section 1.10.32. The standard chunk of Lorem Ipsum used since the
-1500s is reproduced below for those interested. Sections 1.10.32 and
-1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
-reproduced in their exact original form, accompanied by English
-versions from the 1914 translation by H. Rackham.
-`;
-
-export default function Article() {
+export default function Article({ markDown }: { markDown: string }) {
   return (
     <>
       <Header />
@@ -93,7 +46,8 @@ export default function Article() {
             </p>
           </div>
         </div>
-        <ReactMarkdown
+        <MarkdownView
+          markdown={markDown}
           components={{
             h3: (props) => (
               <h3 className="mt-4 text-xl text-left font-play text-orange">
@@ -124,9 +78,7 @@ export default function Article() {
               );
             },
           }}
-        >
-          {markdown}
-        </ReactMarkdown>
+        />
       </article>
       <Footer />
     </>
