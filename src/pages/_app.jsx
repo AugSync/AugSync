8b675744e03a303812f332999de4841c9e0334b2
Augusto Barco <augustopayza@gmail.com>
2022-06-03 16:38:03 -0400

    Add report reportWebVitals

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,25 @@ import '../styles/index.css';
 import { useState, createContext } from 'react';
 import { GlobalStateContext } from 'context/GlobalStateContext';
 
+export function reportWebVitals(metric) {
+  const url = process.env.NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT;
+
+  if (!url) {
+    return;
+  }
+
+  const body = JSON.stringify({
+    route: window.__NEXT_DATA__.page,
+    ...metric,
+  });
+
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url, body);
+  } else {
+    fetch(url, { body, method: 'POST', keepalive: true });
+  }
+}
+
 function App({ Component, pageProps }) {
   const [isSidebarVisible, setIsSidebarVisible] = useState(false);
   function toggleSidebar() {
